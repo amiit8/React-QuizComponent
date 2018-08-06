60b13a949dcd3b0a01e97a24b9a19860216045cf
Amit Upadhyay <amupadhyay@mediaocean.com>
2018-08-06 22:44:42 +0530

    CreateQuiz Chapter 1 part 2 fixed Quiz component

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,10 +10,10 @@ class Quiz extends Component{
 	render(){
 		return (
 			<div>
-				<div className="QuizQuestion">{quizData[0].instruction_text}</div>
+				<div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
 			</div>
 		)
 	}
 }
 
-export default Quiz;
+export default Quiz
